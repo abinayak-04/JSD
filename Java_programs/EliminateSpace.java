a1f4e36890b2b57ebd4e2fb105753e69ef0644a3
abinayak-04 <abinayak0904@gmail.com>
2026-07-30 13:26:28 +0530

    Today's task

--- Java_programs/EliminateSpace.java
+++ Java_programs/EliminateSpace.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EliminateContinueSpace {
+public class EliminateSpace {
     public static void main(String[] args) {
         
         Scanner scan=new Scanner(System.in);
