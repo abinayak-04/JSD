e249f53cbf4629fc6902fe4ee2975c02554cd0f4
abinayak-04 <abinayak0904@gmail.com>
2026-08-01 10:55:00 +0530

    program

--- Java_programs/ReverseNumber.java
+++ Java_programs/ReverseNumber.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
+
 public class ReverseNumber {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         System.out.print("Enter value:");
         long n=scan.nextLong();
-        long rem=0L;
+        long rev=0L;
         while(n!=0){
-            rem=n%10;
-            n=n/10;
-            System.out.print(rem);
+            long rem=n%10;
+            rev=rev*10+rem;
+            n/=10;
         }
-       
+        System.out.println("Reversed Number:"+rev);
         scan.close();
     }
 }
