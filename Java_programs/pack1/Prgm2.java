e21de06733e947b54e59d4ac60f52da5a4464bec
abinayak-04 <abinayak0904@gmail.com>
2026-09-15 11:34:37 +0530

    Program

---
+++ Java_programs/pack1/Prgm2.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class Prgm2 extends Prgm1 {
+    void show(){
+        //System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
---
+++ Java_programs/pack1/Prgm3.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class Prgm3 {
+    public static void main(String[] args) {
+        Prgm1 ob=new Prgm1();
+        //System.out.println(ob.a);
+        System.out.println(ob.b);
+        System.out.println(ob.c);
+        System.out.println(ob.d);
+    }
+}
